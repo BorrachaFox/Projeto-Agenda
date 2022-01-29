3b0857faaaf47e8f3355568adafa6ae4327e7800
BorrachaFox <daviantoniosantos@outlook.com>
2022-01-29 17:26:25 -0300

    create user model config

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,17 +1,17 @@
 exports.middlewareGolbal = (req, res, next) => {
-    res.locals.variavelLocal = 'Este é o valor da variável local.'
-    next()
+  res.locals.errors = req.flash('errors');
+  res.locals.success = req.flash('success');
+  next();
 }
 
 exports.checkCsrfError = (err, req, res, next) => {
-    if(err) {
-        return res.render('404');
-    }
-
-    next();
+  if(err) {
+    return res.render('404');
+  }
+  next();
 }
 
 exports.csrfMiddleware = (req, res, next) => {
-    res.locals.csrfToken = req.csrfToken();
-    next()
+  res.locals.csrfToken = req.csrfToken();
+  next();
 }
