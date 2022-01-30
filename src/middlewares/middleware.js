da3ba60a5e76e8ff16dd8aacdabc1cc6b1bd1230
BorrachaFox <daviantoniosantos@outlook.com>
2022-01-30 13:01:00 -0300

    logando usuario

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,6 +1,8 @@
 exports.middlewareGolbal = (req, res, next) => {
   res.locals.errors = req.flash('errors');
   res.locals.success = req.flash('success');
+  res.locals.user = req.session.user;
+  //res.send(res.locals.user)
   next();
 }
 
